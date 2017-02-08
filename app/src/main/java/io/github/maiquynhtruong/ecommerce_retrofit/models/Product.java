6e781031d802e1ccbb311f279d460ac978bbf3cf
Mai Truong <maitruong2801@gmail.com>
2017-02-07 21:38:03 -0800

    create product object and product service interface

---
+++ app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/models/Product.java
@@ -0,0 +1,8 @@
+package io.github.maiquynhtruong.ecommerce_retrofit.models;
+
+/**
+ * Created by Mai on 2/7/2017.
+ */
+
+public class Product {
+}
---
+++ app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/utils/ProductService.java
@@ -0,0 +1,8 @@
+package io.github.maiquynhtruong.ecommerce_retrofit.utils;
+
+/**
+ * Created by Mai on 2/7/2017.
+ */
+
+public class ProductService {
+}
