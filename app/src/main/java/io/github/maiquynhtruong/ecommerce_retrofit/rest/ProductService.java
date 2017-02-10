7cf120f4f2abb1b7a573ed3e3f3a1768a0e9e1b5
Mai Truong <maitruong2801@gmail.com>
2017-02-10 11:33:03 -0800

    edit the api path

--- app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/rest/ProductService.java
+++ app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/rest/ProductService.java
@@ -1,9 +1,20 @@
 package io.github.maiquynhtruong.ecommerce_retrofit.rest;
 
+import java.util.List;
+
+import io.github.maiquynhtruong.ecommerce_retrofit.models.Product;
+import io.github.maiquynhtruong.ecommerce_retrofit.models.ProductResults;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 /**
  * Created by Mai on 2/7/2017.
  */
 
 public interface ProductService {
+
+    @GET("term={keyword}&key={apiKey}")
+    Call<List<Product>> getProducts(@Path("keyword") String keyword, @Path("apiKey") String apiKey);
 
 }
