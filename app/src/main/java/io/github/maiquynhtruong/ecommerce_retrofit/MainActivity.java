1a85b065b12785f715ed7fb518d1f3f641d0e184
Mai Truong <Mai Truong>
2017-02-19 12:44:51 -0800

    change the retrofit call in MainActivity

--- app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/MainActivity.java
+++ app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/MainActivity.java
@@ -6,7 +6,6 @@ import android.databinding.DataBindingUtil;
 import android.databinding.ObservableArrayList;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,10 +18,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import io.github.maiquynhtruong.ecommerce_retrofit.databinding.ActivityMainBinding;
+import java.util.Arrays;
 import io.github.maiquynhtruong.ecommerce_retrofit.models.Product;
+import io.github.maiquynhtruong.ecommerce_retrofit.models.ProductResults;
 import io.github.maiquynhtruong.ecommerce_retrofit.rest.ProductService;
 import io.github.maiquynhtruong.ecommerce_retrofit.view.adapters.ProductListAdapter;
 import okhttp3.OkHttpClient;
@@ -34,9 +32,10 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView rvProduct;
-    List<Product> productList;
+    ArrayList<Product> productList;
+    ProductListAdapter adapter;
     public static final String API_KEY = "b743e26728e16b81da139182bb2094357c31d331";
-    public static final String API_BASE_URL = "https://api.zappos.com/Search?";
+    public static final String API_BASE_URL = "https://api.zappos.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +55,11 @@ public class MainActivity extends AppCompatActivity {
         rvProduct = (RecyclerView) findViewById(R.id.recycler_view);
         rvProduct.setLayoutManager(new LinearLayoutManager(this));
 
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         Product product = new Product("First", "10.0", "7.5", "1234", "25%", "zappos.com/first", "tumblr.com/image");
         productList.add(product);
-        binding.setProducts(productList);
+        fetchResults("nike");
+//        binding.setProducts(productList);
     }
 
     @Override
@@ -114,32 +114,30 @@ public class MainActivity extends AppCompatActivity {
                 .client(builder.build())
                 .build();
         ProductService service = retrofit.create(ProductService.class);
-        Call<List<Product>> call = service.getProducts(input, API_KEY);
+        Call<ProductResults> call = service.getProducts(input, API_KEY);
 
-        call.enqueue(new Callback<List<Product>>() {
+        call.enqueue(new Callback<ProductResults>() {
             @Override
-            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                productList = response.body();
+            public void onResponse(Call<ProductResults> call, Response<ProductResults> response) {
+                ProductResults products = response.body();
+                productList = new ArrayList<>(Arrays.asList(products.getProducts()));
+
                 if (productList.size() > 0) {
                     for (Product p : productList) {
                         Log.d("Main", p.getProductName());
                     }
-                    showProducts();
+                    adapter = new ProductListAdapter(productList);
+                    rvProduct.setAdapter(adapter);
                 } else {
                     Toast.makeText(getBaseContext(), "No Item found", Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
-            public void onFailure(Call<List<Product>> call, Throwable t) {
+            public void onFailure(Call<ProductResults> call, Throwable t) {
                 Toast.makeText(getBaseContext(), "Failed to get search results. Try again later", Toast.LENGTH_LONG).show();
             }
         });
-    }
-
-    public void showProducts() {
-        ProductListAdapter adapter = new ProductListAdapter(productList);
-        rvProduct.setAdapter(adapter);
     }
 
 }
