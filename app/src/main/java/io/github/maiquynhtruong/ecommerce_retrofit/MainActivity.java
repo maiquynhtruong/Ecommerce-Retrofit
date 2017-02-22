441a9cff18143276eb42c8fc74412b545e164c41
Mai Truong <maitruong2801@gmail.com>
2017-02-21 16:21:17 -0800

    add everything for today

--- app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/MainActivity.java
+++ app/src/main/java/io/github/maiquynhtruong/ecommerce_retrofit/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.maiquynhtruong.ecommerce_retrofit;
 
 import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +12,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -51,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
 //        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         rvProduct = (RecyclerView) findViewById(R.id.recycler_view);
         rvProduct.setLayoutManager(new LinearLayoutManager(this));
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            fetchResults(query);
+        }
         fetchResults("nike");
     }
 
@@ -58,6 +67,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                fetchResults(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                fetchResults(newText);
+                return true;
+            }
+        });
         return true;
     }
 
@@ -67,36 +92,17 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
+            handleSearch();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    public void getUserQuery() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        View view = getLayoutInflater().inflate(R.layout.fragment_search, null);
-        final EditText editText = (EditText) view.findViewById(R.id.edit_query);
-        builder.setTitle("Search").setView(view);
-        builder.setPositiveButton("Search", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String input = editText.getText().toString();
-                fetchResults(input);
-            }
-        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        builder.create().show();
-
+    public void handleSearch() {
+        String string = "nike";
+        fetchResults(string);
     }
-
     public void fetchResults(String input) {
 
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
